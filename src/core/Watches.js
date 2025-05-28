b681c2e14df018d09a65079f1ed141ff8d04d346
fcondolo <fcondolo@gmail.com>
2025-05-28 12:28:28 +0200

    doc update

--- src/core/Watches.js
+++ src/core/Watches.js
@@ -5,62 +5,44 @@ class M68K_Watches {
     ) {
         let t               = this;
         t.entries           = [];
-        t.format            = [
-            // b:bytelen, s:signed
-            {b:0,s:false},  // 0 : error
-            {b:1,s:false},  // 1: WATCH_UBYTE
-            {b:1,s:true},   // 2: WATCH_SBYTE
-            {b:1,s:true},   // 3: WATCH_BYTE
-            {b:2,s:false},  // 4: WATCH_UWORD
-            {b:2,s:true},   // 5: WATCH_SWORD
-            {b:2,s:true},   // 6: WATCH_WORD
-            {b:4,s:false},  // 7: WATCH_ULONG
-            {b:4,s:true},   // 8: WATCH_SLONG
-            {b:4,s:true}    // 9: WATCH_LONG
-        ];
-        t.WATCH_UBYTE      = 1;
-        t.WATCH_SBYTE      = 2;
-        t.WATCH_BYTE       = 3;
-        t.WATCH_UWORD      = 4;
-        t.WATCH_SWORD      = 5;
-        t.WATCH_WORD       = 6;
-        t.WATCH_ULONG      = 7;
-        t.WATCH_SLONG      = 8;
-        t.WATCH_LONG       = 9;
-        t.WATCH_BIN        = 2;     // base
-        t.WATCH_DEC        = 10;    // base
-        t.WATCH_HEX        = 16;    // base
+        t.format            = [];
+        t.WATCH_BYTE        = 1;
+        t.WATCH_WORD        = 2;
+        t.WATCH_LONG        = 3;
+        t.WATCH_BIN         = 2;     // base
+        t.WATCH_DEC         = 10;    // base
+        t.WATCH_HEX         = 16;    // base
         t.hasFocus          = false;
         t.box               = document.getElementById("draggable");
         t.elm               = document.getElementById("tbox_watches");
         WATCHES             = t;
+
+        t.format[0]             = {b:0,s:false}, // b:bytelen, s:signed
+        t.format[t.WATCH_BYTE]  = {b:1,s:false};
+        t.format[t.WATCH_WORD]  = {b:2,s:false};
+        t.format[t.WATCH_LONG]  = {b:4,s:false};
+        
         if (!CODERPARSER_SINGLETON) alert("Create WATCHES singleton after CODERPARSER_SINGLETON");
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_UBYTE", value: 1, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_SBYTE", value: 2, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_BYTE", value: 3, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_UWORD", value: 4, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_SWORD", value: 5, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_WORD", value: 6, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_ULONG", value: 7, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_SLONG", value: 8, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_LONG", value: 9, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_BIN", value: 2, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_DEC", value: 10, path: "", line: "" });
-        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_HEX", value: 16, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_BYTE", value: t.WATCH_BYTE, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_WORD", value: t.WATCH_WORD, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_LONG", value: t.WATCH_LONG, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_BIN", value: t.WATCH_BIN, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_DEC", value: t.WATCH_DEC, path: "", line: "" });
+        CODERPARSER_SINGLETON.constants.push({ name: "WATCH_HEX", value: t.WATCH_HEX, path: "", line: "" });
     }
 
-    add(_name, _adrs, _dataType, _showType) {
+    add(_name, _adrs, _dataType, _base) {
         let t = this;
         let dup = false;
         for (let i = 0; i < t.entries.length; i++) {
             const e = t.entries[i];
-            if ((e.adrs == _adrs) && (e.type == _dataType) && (e.show == _showType) && (e.name == _name)) {
+            if ((e.adrs == _adrs) && (e.type == _dataType) && (e.base == _base) && (e.name == _name)) {
                 dup = true;
                 break;
             }
         }
         if (!dup)
-            t.entries.push({name: _name, adrs: _adrs, type: _dataType, show: _showType});
+            t.entries.push({name: _name, adrs: _adrs, type: _dataType, base: _base});
     }
 
     update() {
@@ -77,14 +59,20 @@ class M68K_Watches {
         for (let i = 0; i < t.entries.length; i++) {
             const e = t.entries[i];
             const f = t.format[e.type];
-            let val = MACHINE.getRAMValue(e.adrs, f.b, false);//f.s);
+            let val = MACHINE.getRAMValue(e.adrs, f.b, f.s);
             const id = "watchval"+i.toString();
             let input = "<input style='max-width:60%' id='" + id + "'";
-            input += "value='" + val.toString(e.show) + "'";
+            input += "value='" + val.toString(e.base) + "'";
             input += "onchange = 'WATCHES.write(" + i + ")'";
             input += "onblur = 'WATCHES.setFocus(false);' onfocus = 'WATCHES.setFocus(true);'";
             input += "/>";
-            s += "<tr><td>" + e.name + "</td><td>" + input + "</td></tr>";
+            let typeStr;
+            switch (e.base) {
+                case t.WATCH_BIN: typeStr = " (bin) "; break;
+                case t.WATCH_DEC: typeStr = " (dec) "; break;
+                case t.WATCH_HEX: typeStr = " (hex) "; break;
+            }
+            s += "<tr><td>" + e.name + typeStr + "</td><td>" + input + "</td></tr>";
         }
         s += "</table>";
         t.elm.innerHTML = s;
@@ -100,7 +88,7 @@ class M68K_Watches {
         const e = t.entries[_index];
         const f = t.format[e.type];
         const v = document.getElementById("watchval"+_index.toString()).value;
-        const num = parseInt(v,e.show);
+        const num = parseInt(v,e.base);
         if (!isNaN(num))
             MACHINE.setRAMValue(num, e.adrs, f.b);
     }
