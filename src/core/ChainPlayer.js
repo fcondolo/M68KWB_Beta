d50c2acbc00cbac6b3203b70ad3bc3bf7c3f0ed3
fcondolo <fcondolo@gmail.com>
2025-05-30 16:51:52 +0200

    Cleanup chain player

---
+++ src/core/ChainPlayer.js
@@ -0,0 +1,55 @@
+const LOCALSTORAGE_CHAININDEX = "chain_index";
+
+var CHAIN_MODE = false;
+
+const chain_fx = [
+  {class: "Non_Regression_Tests", platform: "STE"},
+  {class: "FX_TestParser", platform: "ST"},
+  {class: "Draw2Buffer", platform: "OCS"},
+  {class: "BlitterTornado", platform: "OCS"},
+  {class: "RevisionLogo_v1", platform: "OCS"},
+  {class: "RevisionLogo_v2", platform: "OCS"},
+  {class: "FX_Spherev1", platform: "OCS"},
+  {class: "FX_Spherev2", platform: "OCS"},
+  {class: "FX_Spherev3", platform: "OCS"},
+  {class: "FX_Spherev4", platform: "OCS"},
+  {class: "FX_StarFieldv1", platform: "OCS"},
+  {class: "FX_StarFieldv2", platform: "OCS"},
+  {class: "FX_Heroes", platform: "OCS"},
+  {class: "TestCopper", platform: "OCS"},
+  {class: "AtariSTe_BlitterFill", platform: "STE"},
+  {class: "AtariVBL", platform: "STE"},
+  {class: "tut_Atari_HBL", platform: "STE"},
+  {class: "testAmigaBlitter", platform: "OCS"},
+  {class: "testSTEBlitter", platform: "STE"},
+  {class: "tut_Atari1", platform: "STE"},
+  {class: "tut4_automodified", platform: "STE"},
+  {class: "tut5_AmigaImage", platform: "OCS"},
+  {class: "tut6_STEImage", platform: "STE"},
+];
+
+
+function Check_Chain() {
+  let index = localStorage.getItem(LOCALSTORAGE_CHAININDEX);
+  if (!index)
+    return false;
+  index = parseInt(index);
+  if (!isNaN(index)) {
+    const arrayIndex = index - 1;
+    if (arrayIndex >= 0 && arrayIndex < chain_fx.length) {
+      const classname = chain_fx[arrayIndex].class;
+      if (!main_startChosenFx(classname)) {
+        alert("ERROR: Can't start FX " + classname);
+        return false;
+      }
+      CHAIN_MODE = true;
+      localStorage.setItem(LOCALSTORAGE_CHAININDEX, index + 1);
+      return true;
+    } else  {
+      localStorage.setItem(LOCALSTORAGE_CHAININDEX, null);
+      alert("CHAIN DONE!");
+    }
+  }
+  return false;
+}
+
--- src/core/main.js
+++ src/core/main.js
@@ -5,32 +5,6 @@ const LOCALSTORAGE_FX_PAUSED = "paused";
 const LOCALSTORAGE_FX_ZOOM = "zoom";
 
 
-var CHAIN_MODE = false;
-const chain_fx = [
-  {class: "Non_Regression_Tests", platform: "STE"},
-  {class: "FX_TestParser", platform: "ST"},
-  {class: "Draw2Buffer", platform: "OCS"},
-  {class: "BlitterTornado", platform: "OCS"},
-  {class: "RevisionLogo_v1", platform: "OCS"},
-  {class: "RevisionLogo_v2", platform: "OCS"},
-  {class: "FX_Spherev1", platform: "OCS"},
-  {class: "FX_Spherev2", platform: "OCS"},
-  {class: "FX_Spherev3", platform: "OCS"},
-  {class: "FX_Spherev4", platform: "OCS"},
-  {class: "FX_StarFieldv1", platform: "OCS"},
-  {class: "FX_StarFieldv2", platform: "OCS"},
-  {class: "FX_Heroes", platform: "OCS"},
-  {class: "TestCopper", platform: "OCS"},
-  {class: "AtariSTe_BlitterFill", platform: "STE"},
-  {class: "AtariVBL", platform: "STE"},
-  {class: "tut_Atari_HBL", platform: "STE"},
-  {class: "testAmigaBlitter", platform: "OCS"},
-  {class: "testSTEBlitter", platform: "STE"},
-  {class: "tut_Atari1", platform: "STE"},
-  {class: "tut4_automodified", platform: "STE"},
-  {class: "tut5_AmigaImage", platform: "OCS"},
-  {class: "tut6_STEImage", platform: "STE"},
-];
 
 var SIMU_DEFAULT_WIDTH, PAL_VIDEO_LINES_COUNT, PAL_PLAYFIELD_LINES_COUNT;
 var SIMU_START_BITPLANE, SIMU_END_BITPLANE;
@@ -412,30 +386,6 @@ function main_startChosenFx(_className) {
   return true;
 }
 
-function Check_Chain() {
-  let index = localStorage.getItem("chain_index");
-  if (!index)
-    return false;
-  index = parseInt(index);
-  if (!isNaN(index)) {
-    const arrayIndex = index - 1;
-    if (arrayIndex >= 0 && arrayIndex < chain_fx.length) {
-      const classname = chain_fx[arrayIndex].class;
-      if (!main_startChosenFx(classname)) {
-        alert("ERROR: Can't start FX " + classname);
-        return false;
-      }
-      CHAIN_MODE = true;
-      localStorage.setItem("chain_index", index + 1);
-      return true;
-    } else  {
-      localStorage.setItem("chain_index", null);
-      alert("CHAIN DONE!");
-    }
-  }
-  return false;
-}
-
 
 function findFxIndexFromName(_name) {
   if (_name) {
@@ -450,7 +400,7 @@ function findFxIndexFromName(_name) {
 
 function onFxChosen() {
   if (document.getElementById("searchfx").value.toUpperCase() == "CHAIN") {
-    localStorage.setItem("chain_index", 1);
+    localStorage.setItem(LOCALSTORAGE_CHAININDEX, 1);
     window.location.reload(true);
   }
   hideModalBox();
